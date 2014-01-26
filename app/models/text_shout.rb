cea4457f045afb821ae8c90bea65c99d8d083261
Thomas Roest <thomas_r@live.nl>
2014-01-26 15:38:51 +0100

    add search functionality

--- app/models/text_shout.rb
+++ app/models/text_shout.rb
@@ -1,2 +1,10 @@
 class TextShout < ActiveRecord::Base
+	after_create :create_index
+	after_update :update_index
+
+	def create_index
+	end
+
+	def update_index
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Shouter1::Application.routes.draw do
   root to: 'homes#show', via: :get
   resource :dashboard, only: [:show] #single resource, no id! 
   resource :session, only: [:new, :create, :destroy]
+  resource :search, only: [:show]
   resources :users, only: [:index, :new, :create, :show] do 
   	post 'follow' => 'following_relationships#create'
   	delete 'follow' => 'following_relationships#destroy'
@@ -9,4 +10,5 @@ Shouter1::Application.routes.draw do
   resources :shouts, only: [:show]
   resources :text_shouts, only: [:create]
   resources :photo_shouts, only: [:create]
+  resources :hashtags, only: [:show]
 end
